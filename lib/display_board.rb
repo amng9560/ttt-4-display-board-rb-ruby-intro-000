cee4add202cd8a6220e246a97e0b6c58dff5987b
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-15 12:58:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
+board = ["X", "X", "X", "", "", "", "", "", ""]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+end
+
 # out the current state.
